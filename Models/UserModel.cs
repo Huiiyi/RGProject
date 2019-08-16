3df1dfc48689c0d2abaea90f5c4cce4e48339d52
Huiiyi <39010577+Huiiyi@users.noreply.github.com>
2019-08-16 14:43:07 +0800

    Add ReadMe.md

---
+++ Models/UserModel.cs
@@ -0,0 +1,14 @@
+
+namespace RGProject.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public int Gold { get; set; }
+        public int STR { get; set; }
+        public int DEX { get; set; }
+        public int Lucky { get; set; }
+        public bool DayCheck { get; set; }
+        
+    }
+}
