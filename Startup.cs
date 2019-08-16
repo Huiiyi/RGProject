1c4d76f3187bf2940bf4ccd2eb96ebf4700400b1
Huiiyi <39010577+Huiiyi@users.noreply.github.com>
2019-08-16 13:53:34 +0800

    test

--- Startup.cs
+++ Startup.cs
@@ -48,7 +48,7 @@ namespace RGProject
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            //https
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
