0438f1420401aa83b55d1946dfdf82e13e412cc8
Huiiyi <39010577+Huiiyi@users.noreply.github.com>
2019-08-26 16:11:28 +0800

    Add Database with EF CodeFirst

---
+++ Migrations/20190826080100_Initialize.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RGProject.Migrations
+{
+    public partial class Initialize : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Gold = table.Column<int>(nullable: false),
+                    STR = table.Column<int>(nullable: false),
+                    DEX = table.Column<int>(nullable: false),
+                    Lucky = table.Column<int>(nullable: false),
+                    DayCheck = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
